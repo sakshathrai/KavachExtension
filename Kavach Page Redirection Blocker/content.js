9f0d85235faed8b55324949d74ab038ed2e400eb
Sakshath Rai K <90779367+sakshathrai@users.noreply.github.com>
2024-02-13 15:01:06 +0530

    Add files via upload

---
+++ Kavach Page Redirection Blocker/content.js
@@ -0,0 +1,22 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === "tab_updated") {
+      document.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+          const link = event.target.href;
+          const currentDomain = window.location.hostname;
+  
+          try {
+            const linkDomain = new URL(link).hostname;
+            if (currentDomain !== linkDomain) {
+              const confirmation = confirm(`This link will redirect to ${linkDomain}. Do you want to proceed?`);
+              if (!confirmation) {
+                event.preventDefault(); // Prevent the default link behavior
+              }
+            }
+          } catch (error) {
+            console.error("Error processing link:", error);
+          }
+        }
+      });
+    }
+  });
